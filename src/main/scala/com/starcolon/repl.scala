443c1b3abfd203dada7e03f69794007c571e9d79
starcolon <tao@starcolon.com>
2017-11-21 22:39:36 +0700

    adding repl

---
+++ src/main/scala/com/starcolon/repl.scala
@@ -0,0 +1,19 @@
+package com.starcolon.tao.repl
+
+import com.starcolon.tao.forex.endpoint.ForexEndPoint
+
+object CoreREPL extends App  {
+
+  println("**************************")
+  println
+  println("    Forex API Endpoint    ")
+  println
+  println("**************************")
+    
+  repl()
+
+
+  def repl(){
+
+  }
+}
